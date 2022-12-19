690a0a0b4959551f29c432f3db5738692785ac82
marcbo <marcbo@student.ethz.ch>
2022-12-19 01:41:24 +0100

    add test from other job

---
+++ preprocess/srcml2.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#SBATCH --mail-type=ALL
+#SBATCH --mem=64G
+#SBATCH --output=/home/marcbo/dataprep/log/%j.out
+#SBATCH --error=/home/marcbo/dataprep/log/%j.err
+#SBATCH --nodelist=tikgpu03
+
+#Job script to test astattendgru preprocessing
+
+# Exit on errors
+set -o errexit
+
+# Set a directory for temporary files unique to the job with automatic removal at job termination
+TMPDIR=$(mktemp -d)
+if [[ ! -d ${TMPDIR} ]]; then
+            echo 'Failed to create temp directory' >&2
+                exit 1
+fi
+trap "exit 1" HUP INT TERM
+trap 'rm -rf "${TMPDIR}"' EXIT
+export TMPDIR
+
+# Change the current directory to the location where you want to store temporary files, exit if changing didn't succeed.
+# Adapt this to your personal preference
+cd "${TMPDIR}" || exit 1
+
+# Activate the conda environment
+#source /home/marcbo/.bashrc
+[[ -f /itet-stor/marcbo/net_scratch/conda/bin/conda ]] && eval "$(/itet-stor/marcbo/net_scratch/conda/bin/conda shell.bash hook)"
+conda activate astgru
+echo "Conda activated"
+
+# Send some noteworthy information to the output log
+echo "Running on node: $(hostname)"
+echo "In directory:    $(pwd)"
+echo "Starting on:     $(date)"
+echo "SLURM_JOB_ID:    ${SLURM_JOB_ID}"
+
+
+# Run on arton
+sbatch /home/marcbo/astgru/funcom/preprocess/srcml.sh
+
+# Send more noteworthy information to the output log
+echo "Finished at:     $(date)"
+
+# End the script with exit code 0
+exit 0
